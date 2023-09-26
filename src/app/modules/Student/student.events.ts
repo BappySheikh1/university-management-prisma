af1e3c27f6872ce0f1d615de5de89a0bc728476a
BappySheikh1 <sheikhbappy91gmail.com>
2023-09-26 17:14:42 +0600

    StudentUpdated event

--- src/app/modules/Student/student.events.ts
+++ src/app/modules/Student/student.events.ts
@@ -7,6 +7,11 @@ const initStudentEvents = () => {
     const data = JSON.parse(e);
     await StudentService.createStudentFromEvent(data);
   });
+
+  RedisClient.subscribe(EVENT_STUDENT_CREATED, async (e: string) => {
+    const data = JSON.parse(e);
+    await StudentService.updateStudentFromEvent(data);
+  });
 };
 
 export default initStudentEvents;
